86eb20db2451d30a4506a795d2fa1163b4d7598c
windingtheropes <17016045+windingtheropes@users.noreply.github.com>
2021-01-19 20:48:32 -0500

    Update coolio.cs

--- coolio.cs
+++ coolio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+//coolio created by windingtheropes
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,7 @@ namespace coolio
 {
     class coolio
     {
-        //2021 coolio for c# applications created by windingtheropes
-        //cooliomakes IO parts of console apps easier to use
-        //see documentation above functions for more specific information
-
-        //prints text
-        //types:
-        //0 = automatically jumps to the next line
-        //1 = require keypress to return to next line
+     
         public void print(string message, int type)
         {
             if (type == 0)
@@ -31,11 +25,7 @@ namespace coolio
             }
 
         }
-        //gets user input, with a set message. automatically adds a space between your message and the text input. 
-        //types:
-        //0 = input: type here
-        //1 = input:
-        //    type here
+     
         public string input(string message, int type)
         {
             if (type == 0)
